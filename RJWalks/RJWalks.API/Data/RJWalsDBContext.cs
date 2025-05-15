2bf58a4505a8756ca218e84de9fc2d360511f0de
Maria Eduarda Nóbrega Santos <maria.santos@gestqual.com.br>
2025-05-15 16:02:08 -0300

    Adicionar .gitignore, remove arquivos temporarios e add connectionstrings

---
+++ RJWalks/RJWalks.API/Data/RJWalsDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RJWalks.API.Models.Domain;
+
+namespace RJWalks.API.Data
+{
+    public class RJWalsDBContext : DbContext
+    {
+        public RJWalsDBContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+        {
+            
+        }
+
+        //All of these properties represens collection inside our database
+        public DbSet<Difficulty> Difficulties { get; set; }
+
+        public DbSet<Region> Regions { get; set; }
+
+        public DbSet<Walk> Walks { get; set; }
+
+    }
+}
